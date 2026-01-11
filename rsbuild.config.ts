007e9a9d066cea8bd5c626ea40e903df7ac370ff
p-viral <pviral1712@gmail.com>
2026-01-11 09:01:03 -0500

    Setup github actions for static site hosting

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -3,5 +3,18 @@ import { pluginReact } from '@rsbuild/plugin-react';
 
 // Docs: https://rsbuild.rs/config/
 export default defineConfig({
+  html: {
+    title: 'Mortgage Calculator',
+  },
+  server: {
+    base: process.env.PUBLIC_BASE_PATH ?? '/',
+  },
+  output: {
+    assetPrefix:
+      process.env.PUBLIC_BASE_PATH ??
+      (process.env.GITHUB_REPOSITORY
+        ? `/${process.env.GITHUB_REPOSITORY.split('/')[1] ?? ''}/`
+        : '/'),
+  },
   plugins: [pluginReact()],
 });
